b473faffa70d2981d0cc27f93d6b7fcf464309f2
fravila08 <fr4v1l4@gmail.com>
2024-03-14 08:47:13 -0700

    part-II

--- start/store.sql
+++ start/store.sql
@@ -1,3 +1,25 @@
+DROP TABLE IF EXISTS action_figure;
+
+CREATE TABLE action_figure(
+    id SERIAL PRIMARY KEY,
+    action_figure_title VARCHAR(30),
+    quantity INT,
+    price DECIMAL(4,2)
+);
+
+\COPY action_figure FROM './data/action_figure.csv' WITH CSV HEADER;
+
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE employee(
+    id SERIAL PRIMARY KEY,
+    employee_name VARCHAR(30),
+    position VARCHAR(50),
+    salary DECIMAL(7,2)
+);
+
+\COPY employee FROM './data/employee.csv' WITH CSV HEADER;
+
 -- Drop the table if it exists
 DROP TABLE IF EXISTS game;
 
@@ -12,3 +34,14 @@ CREATE TABLE game (
 
 -- Insert Sample Data into Game Table
 \COPY game FROM './data/game.csv' WITH CSV HEADER;
+
+DROP TABLE IF EXISTS poster;
+
+CREATE TABLE poster(
+    id SERIAL PRIMARY KEY,
+    poster_title VARCHAR(30),
+    quantity INT,
+    price DECIMAL(4,2)
+);
+
+\COPY poster FROM './data/poster.csv' WITH CSV HEADER;
